34e44933b205fa26b80b889a209e88e78ab8e52b
kosarfz <45241146+kosarfz@users.noreply.github.com>
2020-09-06 11:45:51 +0430

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,43 @@
+/* ----- Navigation scroll ----- */
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a:not(.btn-floating)").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top - 80,
+        },
+        900,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash + 80;
+        }
+      );
+    } // End if
+  });
+});
+
+/* ----- Sidebar Dropdown Item ----- */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
